1e7f1fdf78d2f3443c9fb6eea7e3c4274571b5d5
Mauricio Flores Hernández <mau1361317@gmail.com>
2019-05-06 17:43:40 -0500

    Base application

---
+++ app/src/main/java/com/devmaufh/degreedaysapp/Fragments/ListFragment.java
@@ -0,0 +1,24 @@
+package com.devmaufh.degreedaysapp.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.devmaufh.degreedaysapp.R;
+
+public class ListFragment extends Fragment {
+
+    public ListFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/devmaufh/degreedaysapp/MainActivity.java
@@ -0,0 +1,45 @@
+package com.devmaufh.degreedaysapp;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.view.MenuItem;
+
+import com.devmaufh.degreedaysapp.Fragments.HomeFragment;
+import com.devmaufh.degreedaysapp.Fragments.ListFragment;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNav;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment=null;
+                switch (menuItem.getItemId()){
+                    case R.id.homemenu_home:
+                        fragment=new HomeFragment();break;
+                    case R.id.homemenu_registros:
+                        fragment= new ListFragment();break;
+                    case R.id.homemenu_perfil:
+                        fragment=null;break;
+                }
+                setFragment(fragment);
+                return true;
+            }
+        });
+    }
+    private void bindUI(){
+        bottomNav=(BottomNavigationView)findViewById(R.id.home_BottomNav);
+    }
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_home,fragment);
+        fragmentTransaction.commit();
+    }
+}
